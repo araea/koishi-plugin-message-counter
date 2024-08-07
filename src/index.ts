fcce0fa4691979634117ebafe81076b565161338
Nawyjx <3373167460@qq.com>
2024-08-07 14:19:25 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -967,7 +967,16 @@ export async function apply(ctx: Context, config: Config) {
 
       // 如果 name 不存在，根据 userId 获取相应的 name
       if (!name) {
-        const guildMember = await session.bot.getGuildMember(session.guildId, userId);
+        let guildMember;
+        try {
+          guildMember = await session.bot.getGuildMember(session.guildId, userId);
+        } catch (error) {
+          guildMember = {
+            user: {
+              name: '未知用户',
+            },
+          };
+        }
 
         // 替换原始的 at 标签
         const newAtTag = `<at id="${userId}" name="${guildMember.user.name}"/>`;
